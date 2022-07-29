d2e96151bf3562ac9de2054a95362600ef601c8d
Gabriel Valle <gabrielvallerodrigues@gmail.com>
2022-07-29 13:27:57 -0300

    Finalizada aula 2

---
+++ routes/produtos.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+//RETORNA TODOS OS PRODUTOS
+router.get('/', (req, res, next)=>{
+    res.status(200).send({
+        mensagem: 'retorna os produtos'
+    });
+}); 
+
+//INSERE UM PRODOTO
+router.post('/', (req, res, next)=>{
+    res.status(201).send({
+        mensagem: 'Produto criado'
+    });
+}); 
+
+//RETORNA OS DADOS DE UM PRODUTO
+router.get('/:id_produto', (req, res, next) =>{
+    const id = req.params.id_produto;
+    res.status(200).send({
+        mensagem: 'Detalhes do produto',
+        id: id
+    })
+})
+
+//ALTERA UM PRODUTO
+router.patch('/', (req, res, next)=>{
+    res.status(201).send({
+        mensagem: 'Editar produtos'
+    });
+}); 
+
+//DELETA UM PRODUTO
+router.delete('/', (req, res, next)=>{
+    res.status(201).send({
+        mensagem: 'Produto excluido'
+    });
+}); 
+
+module.exports = router;
